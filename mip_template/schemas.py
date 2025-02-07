ba6fb682d5f821f8756d21e4df2bfcfda5d64bea
Ruan Felipe <ruanfelipe@matematica.ufrj.br>
2025-02-07 09:48:31 -0300

    Updates schemas.py with the data scheme and integrity restrictions.

--- mip_template/schemas.py
+++ mip_template/schemas.py
@@ -3,7 +3,7 @@ Defines the input and output schemas of the problem.
 For more details on how to implement and configure data schemas see:
 https://github.com/mipwise/mip-go/tree/main/5_develop/4_data_schema
 """
-from mwcommons.ticdat_types import float_number, positive_float, positive_integer, text, non_negative_float
+from mwcommons.ticdat_types import positive_integer, text, binary
 from ticdat import PanDatFactory
 
 from mip_template.constants import SampleConstants
@@ -11,36 +11,32 @@ from mip_template.constants import SampleConstants
 
 # region INPUT SCHEMA
 input_schema = PanDatFactory(
-    parameters=[['Name'], ['Value']],  # Do not change the column names of the parameters table!
-    sample_input_table=[['Primary Key One', 'Primary Key Two'], ['Data Field One', 'Data Field Two']],
+    parameters   = [['Name'], ['Value']],  # Do not change the column names of the parameters table!
+    person_lectures = [['Name', 'Symposium_ID'], ['Organizer', 'University', 'Email', 'Subject', 'Main tag', 'Secondary tag']],
+    symposiums = [['Symposium_ID'], ['Organizer', 'Blocks', 'Title',]]
 )
-# endregion
 
-# region USER PARAMETERS
-input_schema.add_parameter('Sample Text Parameter', default_value='Any Text', **text())
-input_schema.add_parameter('Sample Two Values Parameter', default_value=SampleConstants.FIRST_VALUE,
-                           **text(strings_allowed=tuple(SampleConstants)))
-input_schema.add_parameter('Sample Float Parameter', default_value=1.5,
-                           **non_negative_float(max=10, inclusive_max=True))
-input_schema.add_parameter('Sample Int Parameter', default_value=1,
-                           **non_negative_float(max=10, inclusive_max=True))
-input_schema.add_parameter('Sample Date Parameter', default_value='11/21/2022', datetime=True)
-# endregion
+## Restrições de integridade das tabelas
+table = 'persons_lectures'
+input_schema.set_data_type(table=table, field='Name', **text())
+input_schema.set_data_type(table=table, field='Symposium_ID', **positive_integer(min=0))
+input_schema.set_data_type(table=table, field='Organizer', **binary())
+input_schema.set_data_type(table=table, field='University', **text())
+input_schema.set_data_type(table=table, field='Email', **text())
+input_schema.set_data_type(table=table, field='Subject', **text())
+input_schema.set_data_type(table=table, field='Main tag', **text())
+input_schema.set_data_type(table=table, field='Secondary tag', **text())
+
+table = 'symposiums'
+input_schema.set_data_type(table=table, field='Symposium_ID', **positive_integer(min=0))
+input_schema.set_data_type(table=table, field='Organizer', **binary())
+input_schema.set_data_type(table=table, field='Blocks', **positive_integer(min=1, inclusive_min=True))
+input_schema.set_data_type(table=table, field='Title', **text()) 
 
 # region OUTPUT SCHEMA
 output_schema = PanDatFactory(
     sample_output_table=[['Primary Key'], ['Data Field']],
 )
-# endregion
-
-# region DATA TYPES AND PREDICATES - INPUT SCHEMA
-# region sample_input_table
-table = 'sample_input_table'
-input_schema.set_data_type(table=table, field='Primary Key One', **text())
-input_schema.set_data_type(table=table, field='Primary Key Two', **text())
-input_schema.set_data_type(table=table, field='Data Field One', **text(strings_allowed=('Option 1', 'Option 2')))
-input_schema.set_data_type(table=table, field='Data Field Two', **float_number())
-# endregion
 # endregion
 
 # region DATA TYPES AND PREDICATES - OUTPUT SCHEMA
