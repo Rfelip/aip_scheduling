c50bb693e37c0f101d69cbd365f7c483dcb1419a
Eder Pinheiro <eder.pinheiro@mipwise.com>
2025-05-23 14:11:08 -0300

    feature: change input schema

--- aip_scheduling/schemas.py
+++ aip_scheduling/schemas.py
@@ -5,41 +5,39 @@ https://github.com/mipwise/mip-go/tree/main/5_develop/4_data_schema
 """
 from mwcommons.ticdat_types import positive_integer, text, binary
 from ticdat import PanDatFactory
+input_schema = PanDatFactory(
+    parameters   = [['Name'], ['Value']],  # Do not change the column names of the parameters table!
+    minisymposiums = [['Minisymposium ID'],['Nome', 'Quantidade de Blocos']],
+    participants = [['Participante ID'], ['Nome', 'Universidade', 'País']],
+    lectures = [['Palestra ID'], ['Minisymposium ID']],
+    participants_minisymposums = [['Participante ID', 'Minisymposium ID'], ['Função']],
+    sessions = [['Sessão ID'], ['Description', 'Dia', 'Horário', 'Quantidade de Salas']],# Salas == Paralelas
+    session_weights = [["Sessão Precedente", "Sessão Sequente"], ["Peso"]],
+    rooms_weights = [['Sala Precedente', 'Sala Sequente'], ['Peso']],
+)
 
 from aip_scheduling.constants import SampleConstants
 
 
 # region INPUT SCHEMA
-input_schema = PanDatFactory(
-    parameters   = [['Name'], ['Value']],  # Do not change the column names of the parameters table!
-    minisymposiums = [['ID'],['Nome', 'Blocks']],
-    person = [["Nome"], ['Universidade', 'País', 'Palestra 1','Palestra 2', 'Organiza 1', 'Organiza 2']],
-    session_weights = [["origem"],["sessão 1","sessão 2","sessão 3","sessão 4","sessão 5","sessão 6",
-                       "sessão 7","sessão 8","sessão 9"]],
-    paralelas_weights = [["origem"],["paralela 1","paralela 2","paralela 3","paralela 4","paralela 5",
-                               	"paralela 6","paralela 7","paralela 8","paralela 9","paralela 10"]],
-)
 
 ## Restrições de integridade das tabelas
 table = 'minisymposiums'
-input_schema.set_data_type(table=table, field='ID', **positive_integer(min=1, inclusive_min=True))
+input_schema.set_data_type(table=table, field='Minisymposium ID', **text())
 input_schema.set_data_type(table=table, field='Nome', **text())
-input_schema.set_data_type(table=table, field='Blocks', **positive_integer(min=1, inclusive_min=True))
+input_schema.set_data_type(table=table, field='Quantidade de Blocos', **positive_integer(min=1, inclusive_min=True))
 
-table = 'symposiums'
-input_schema.set_data_type(table=table, field='Symposium_ID', **positive_integer(min=1, inclusive_min=True))
-input_schema.set_data_type(table=table, field='Organizer', **text())
-input_schema.set_data_type(table=table, field='Blocks', **positive_integer(min=1, inclusive_min=True))
-input_schema.set_data_type(table=table, field='Title', **text()) 
-
-table = "person"
+table = "participants"
+input_schema.set_data_type(table=table, field='Participante ID', **text())
 input_schema.set_data_type(table=table, field='Nome', **text())
 input_schema.set_data_type(table=table, field='Universidade', **text())
 input_schema.set_data_type(table=table, field='País', **text())
-input_schema.set_data_type(table=table, field='Palestra 1', **positive_integer(min=1, inclusive_min=True))
-input_schema.set_data_type(table=table, field='Palestra 2', **positive_integer(min=1, inclusive_min=True))
-input_schema.set_data_type(table=table, field='Organiza 1', **positive_integer(min=1, inclusive_min=True))
-input_schema.set_data_type(table=table, field='Organiza 2', **positive_integer(min=1, inclusive_min=True))
+
+table = "lectures"
+input_schema.set_data_type(table=table, field='Palestra ID', **text())
+input_schema.set_data_type(table=table, field='Minisymposium ID', **text())
+input_schema.add_foreign_key(native_table=table, foreign_table='minisymposiums', mappings=['Minisymposium ID', 'Minisymposium ID'])
+
 
 table = "session_weights"
 input_schema.set_data_type(table=table, field='origem', **text())
